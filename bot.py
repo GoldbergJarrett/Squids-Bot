9f02ab297c4529f30d182be9ebb3cb05526460ee
Jarrett Goldberg <goldbergjarrett@gmail.com>
2020-04-07 21:51:20 -0400

    clean up and added comments

--- bot.py
+++ bot.py
@@ -3,29 +3,32 @@ from discord.ext import commands
 
 bot = commands.Bot(command_prefix='!')
 
-#Oauth
-
+#test command
 @bot.command()
 async def ping(ctx):
     await ctx.send(f'Pong! {round(client.latency * 1000)}ms')
 
+
 @bot.command()
 async def plex(ctx, *, movie: str):
-
+'''
+command to add movies to a plex movie server list. Use it by doing "!plex 'movie name'". It will add 'movie name' to the list of movies.
+'''
     exists = False
-
+    #check to see if the file is already on the list of movies to be added to the plex.
     with open('new.txt', 'r') as newFile:
         for line in newFile:
             if movie in line:
                 exists = True
                 await ctx.send("Movie is already on the list to be added to the Plex!")
-
+    #check to see if the movie is already on the plex
     with open('plex.txt', 'r') as plexFile:
         for line in plexFile:
             if movie in line:
                 exists = True
                 await ctx.send("Movie is already on the Plex!")
-
+                
+    #adds movie to the list of plex movies to be added.
     if exists is False:
         with open('new.txt', 'a') as newF:
             newF.write(movie + '\n')
@@ -35,4 +38,4 @@ async def plex(ctx, *, movie: str):
 async def test(ctx, *, movie: str):
     await ctx.send(movie)
     
-clinet.run('')
+client.run('')
